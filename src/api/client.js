7bb335bb84e6f17fd3070f24cc5bf65a8a0cf143
Gaston Herrlein <herrleingaston@gmail.com>
2024-04-22 20:05:25 +0200

    feat: se agrega comunicacion con la api

---
+++ src/api/client.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const client = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+client.interceptors.response.use((response) => response.data);
+
+export const setAuthorizationHeader = (token) =>
+  (client.defaults.headers.common["Authorization"] = `Bearer ${token}`);
